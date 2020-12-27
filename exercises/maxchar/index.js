28bb14577c9a431f661b363986dbb35ff503baf8
Subarna Karki <subarna.karki13@gmail.com>
2020-12-27 17:30:04 -0500

    complete max characer exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let counter = {};
+  let maxCount = 0;
+  let maxChar = '';
+  for (const letter of str) {
+    !counter[letter] ? counter[letter] = 1 :  counter[letter] +=1;
+  }
+
+  for (const char in counter) {
+    if (counter[char] > (maxCount) ) {
+      maxChar = char;
+      maxCount = counter[char]
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
